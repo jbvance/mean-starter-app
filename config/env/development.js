4977da6e5ce89dce59a59db163b51959922e716a
Jason Vance <jbvance@gmail.com>
2017-01-31 17:47:41 -0600

    Update config variables

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,6 @@
 module.exports = {
   //Development configuration options
+  //NOT CURRENTLY USED - using environment variables instead
   db: 'mongodb://localhost/mean-book',
 
   sessionSecret: ''
--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,8 @@ const config = require ('./config');
 const mongoose = require ('mongoose')
 
 module.exports = function(){
-    const db = mongoose.connect(config.db);
+    //const db = mongoose.connect(config.db);
+    const db = mongoose.connect(process.env.DB_URI);
 
     require('../app/models/user.server.model');
 
