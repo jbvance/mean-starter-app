6cffb5980dd46425f9e751cb462be828448d2c4c
Jason Vance <jbvance@gmail.com>
2017-02-03 16:50:11 -0600

    Partially complete reset password

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,6 +1,9 @@
 // Load the module dependencies
 const User = require('mongoose').model('User');
 const passport = require('passport');
+const async = require('async');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
 
 // Create a new error handling controller method
 const getErrorMessage = function(err) {
@@ -160,3 +163,66 @@ exports.signout = function(req, res) {
 	// Redirect the user back to the main application page
 	res.redirect('/');
 };
+
+exports.renderForgotPassword = function(req, res) {
+  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+    if (!user) {
+      req.flash('error', 'Password reset token is invalid or has expired.');
+      return res.redirect('/forgot');
+    }
+    res.render('reset', {
+      user: req.user
+    });
+  });
+};
+
+exports.sendPasswordResetEmail = function(req, res, next) {
+  async.waterfall([
+    function(done) {
+      crypto.randomBytes(20, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+    function(token, done) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'No account with that email address exists.');
+          return res.redirect('/forgot');
+        }
+
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+        user.save(function(err) {
+          done(err, token, user);
+        });
+      });
+    },
+    function(token, user, done) {
+      var smtpTransport = nodemailer.createTransport( {
+        service: 'SendGrid',
+        auth: {
+          user: '!!! YOUR SENDGRID USERNAME !!!',
+          pass: '!!! YOUR SENDGRID PASSWORD !!!'
+        }
+      });
+      var mailOptions = {
+        to: user.email,
+        from: 'passwordreset@demo.com',
+        subject: 'Node.js Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+        done(err, 'done');
+      });
+    }
+  ], function(err) {
+    if (err) return next(err);
+    res.redirect('/forgot');
+  });
+}
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,7 +1,5 @@
 const users = require('../../app/controllers/users.server.controller');
 const passport = require('passport');
-const nodemailer = require('nodemailer');
-const async = require('async');
 
 module.exports = function(app) {
   app.route('/signup')
@@ -21,70 +19,20 @@ module.exports = function(app) {
 
   //Forgot password functionality
 
-  //Render the form to enter a new password if user is found
-  app.get('/reset/:token', function(req, res) {
-    User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-      if (!user) {
-        req.flash('error', 'Password reset token is invalid or has expired.');
-        return res.redirect('/forgot');
-      }
-      res.render('reset', {
-        user: req.user
-      });
+  app.get('/forgot', function(req, res) {
+    res.render('forgot', {
+      user: req.user,
+      title: "Reset Password",
+      // Set the flash message variable
+			messages: req.flash('error') || req.flash('info')
     });
   });
 
   //Process request to send password reset email
-  app.post('/forgot', function(req, res, next) {
-    async.waterfall([
-      function(done) {
-        crypto.randomBytes(20, function(err, buf) {
-          var token = buf.toString('hex');
-          done(err, token);
-        });
-      },
-      function(token, done) {
-        User.findOne({ email: req.body.email }, function(err, user) {
-          if (!user) {
-            req.flash('error', 'No account with that email address exists.');
-            return res.redirect('/forgot');
-          }
+  app.post('/forgot', users.sendPasswordResetEmail);
 
-          user.resetPasswordToken = token;
-          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-
-          user.save(function(err) {
-            done(err, token, user);
-          });
-        });
-      },
-      function(token, user, done) {
-        var smtpTransport = nodemailer.createTransport('SMTP', {
-          service: 'SendGrid',
-          auth: {
-            user: '!!! YOUR SENDGRID USERNAME !!!',
-            pass: '!!! YOUR SENDGRID PASSWORD !!!'
-          }
-        });
-        var mailOptions = {
-          to: user.email,
-          from: 'passwordreset@demo.com',
-          subject: 'Node.js Password Reset',
-          text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-            'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-        };
-        smtpTransport.sendMail(mailOptions, function(err) {
-          req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-          done(err, 'done');
-        });
-      }
-    ], function(err) {
-      if (err) return next(err);
-      res.redirect('/forgot');
-    });
-  });
+  //Render the form to enter a new password if user is found
+  app.get('/reset/:token', users.renderForgotPassword);
 
 
 };
