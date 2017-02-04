6c84a59c7af7c1355123786c985b18f3fd7cfe51
Jason Vance <jbvance@gmail.com>
2017-02-04 08:29:31 -0600

    Continue reset password implementation

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -4,6 +4,8 @@ const passport = require('passport');
 const async = require('async');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
+const Mailgun = require('mailgun-js');
+
 
 // Create a new error handling controller method
 const getErrorMessage = function(err) {
@@ -200,26 +202,35 @@ exports.sendPasswordResetEmail = function(req, res, next) {
       });
     },
     function(token, user, done) {
-      var smtpTransport = nodemailer.createTransport( {
-        service: 'SendGrid',
-        auth: {
-          user: '!!! YOUR SENDGRID USERNAME !!!',
-          pass: '!!! YOUR SENDGRID PASSWORD !!!'
-        }
-      });
-      var mailOptions = {
-        to: user.email,
-        from: 'passwordreset@demo.com',
-        subject: 'Node.js Password Reset',
-        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-      };
-      smtpTransport.sendMail(mailOptions, function(err) {
-        req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-        done(err, 'done');
-      });
+
+      //Mailgun info
+      //Your api key, from Mailgun’s Control Panel
+      var api_key = process.env.MAILGUN_API_KEY;
+
+      //Your domain, from the Mailgun Control Panel
+      var domain = process.env.MAILGUN_DOMAIN
+
+      //Your sending email address
+      var from_who = 'admin@texasestatedocs.com';
+
+      //We pass the api_key and domain to the wrapper, or it won't be able to identify + send emails
+     var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+
+     var data = {
+     //Specify email data
+       from: from_who,
+     //The email to contact
+       to: req.body.email,
+     //Subject and text data
+       subject: 'Hello from Texas Estate Docs',
+       html: 'Hello, This is not a plain-text email, I wanted to test some spicy Mailgun sauce in NodeJS! <a href="http://0.0.0.0:3030/validate?' + req.params.mail + '">Click here to add your email address to a mailing list</a>'
+     }
+
+     //Invokes the method to send emails given the above data with the helper library
+     mailgun.messages().send(data, function (err, body) {
+         req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+         done(err, 'done');
+     });
     }
   ], function(err) {
     if (err) return next(err);
